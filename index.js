4cd317524fc1a69100c257de8df0d80b57ecc6c1
Kailany7 <Kailanysilva118@gmail.com>
2024-09-14 21:52:35 -0300

    fundamentos 003

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello world")
+let meta = {
+    value: ' ler um livro por mês',
+    checked: true,
+}
+
+let metas = [
+    meta,
+    {
+        value: "caminhar 20 minutos todos os dias",
+        checked: false
+    }
+]
+
+console.log(metas[1].value)
