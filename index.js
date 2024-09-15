9ac164871552be5b4c564bbcdb4b5e50c40d1c5e
Kailany7 <Kailanysilva118@gmail.com>
2024-09-15 18:19:12 -0300

    lsitar metas

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
-const { select, input } = require('@inquirer/prompts') // para extrair algum módulo pronto
+const { select, input, checkbox } = require('@inquirer/prompts') // para extrair algum módulo pronto
 
 let meta = {
     value: 'Toamr 3l de água por dia',
     checked: false
 }
 
-let metas = [ meta ]  // lista de metas
+let metas = [ meta ]  
 
 const cadastraMetas = async () => {
     const meta = await input({ message: "Digite a meta:" }) 
     
-    if(meta.length == 0){ // length é para ver quantos caracteres foram escritos, nesse caso para o if funcionar tem que ser Zero caracteres
-        console.log('A meta não pode ser vazia')
+    if(meta.length == 0){
         return
     }
 
-    metas.push( // serve para colocar mais metas
+    metas.push( 
         { value: meta, checked: false}
     )
 } 
 
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message: "Use as Setas para mudar de meta, o Espaço para marcar ou desmarcar e o Enter para finalizar essa etapa.",
+       //choices é uma escola e colocamos uma array : []
+        choices: [...metas], // colocamos as metas dentro da lista que podemos imaginar como uma caixa e os ... fez uma copia d emeta e espalhou na "caixa"
+        instructions: false, // limpa a instrução em ingles que aparece
+    })
 
+    if(respostas.length == 0){
+        console.log("Nenhuma meta selecionada!")
+        return
+    }
+    // mecanismo para desmarcar as metas
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    respostas.forEach((resposta) => {  //forEach (para cada) ver o que o usuario digitou e compara com as metas
+         const meta = metas.find((m)=> {  //find ver as metas já feitas para comparar com a meta digitada
+             return m.value == resposta
+         }) 
+         meta.checked = true
+    })
+
+    console.log('Meta(s) marcadas como concluída(s)')
+}
 
 
 
@@ -56,7 +80,7 @@ const start = async () => {
                 console.log(metas)
                 break
             case "listar":
-                console.log("vamos listar")
+                await listarMetas() 
                 break
             case "sair":
                 console.log("Até a próxima")
